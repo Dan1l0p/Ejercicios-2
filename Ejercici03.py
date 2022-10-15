faaf6b8a2f379b5ed4c39a484998d8f39d3b0e55
Dan1l0p <dalova2003@gmail.com>
2022-10-15 19:58:33 +0200

    e3

--- Ejercici03.py
+++ Ejercici03.py
@@ -1,21 +1,22 @@
 def modificar(lista):
-    nueva_lista = []
-    nueva_lista = set(lista)
+
+    nueva_lista = list(set(lista))
+    nueva_lista.sort(reverse=True)
+    
+    nueva_lista2 = nueva_lista
     for num in lista:
         if not num % 2 == 0:
-                nueva_lista.remove(num)
+            nueva_lista2.pop(num)
         else:
-                nueva_lista.append(num)
-
-        return nueva_lista
-
-    lista_inver = nueva_lista.sort(reverse=True)
-    return nueva_lista
+            nueva_lista2.append(num)
+    suma = sum(nueva_lista2)
+    nueva_lista2.insert(0,suma)
+    return nueva_lista2
     
 
 
 
 lista = [1,2,3,4,5,5,4,8,9,2,]
-nueva_lista= modificar(lista)
+nuevalista= modificar(lista)
 print(modificar(lista))
-print(lista)
+print(nuevalista[0]== sum(nuevalista[1:]))
