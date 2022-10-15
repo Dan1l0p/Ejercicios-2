d5451301ed4d56a53591b88ebf17b18284048ebc
Dan1l0p <dalova2003@gmail.com>
2022-10-15 21:51:00 +0200

    fin 2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,4 +1,11 @@
 texto = "un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro"
 
 frases = texto.split("#")
-print(frases)
+for i, frase in enumerate(frases):
+    frases[i]= frase.capitalize()
+    if i == 0:
+        frases[i] = frases[i] + "..."
+    else:
+        frases[i] = "-" + frases[i]+"."
+for frase in frases:
+    print(frase)
